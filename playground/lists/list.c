fcfc4b00d7dde2d192d56d5cfd887c00c160a589
João Mário Fidelis Martins <joaomf17p2@gmail.com>
2025-07-06 17:07:28 -0300

    adding playground file

---
+++ playground/lists/list.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../../data-structures/lists/double-linked/double-linked-list.h"
+
+ELEMENT createElement(KEY key)
+{
+  RECORD new_record;
+
+  new_record.key = key;
+
+  ELEMENT new_element;
+
+  new_element.next = NULL;
+  new_element.prev = NULL;
+  new_element.record = new_record;
+
+  return new_element;
+}
+
+void show (LIST *list)
+{
+  POINTER currenct_reference = list->init; 
+
+  printf("My list: ");
+
+  do
+  {
+    printf("%i ", currenct_reference->record.key);
+    currenct_reference = currenct_reference->next;
+  }
+  while (currenct_reference != NULL);
+
+  printf("\n");
+}
+
+int main (void)
+{
+  LIST list;
+
+  init(&list);
+
+  ELEMENT element_1 = createElement(2);
+  ELEMENT element_2 = createElement(4);
+  ELEMENT element_3 = createElement(1);
+  ELEMENT element_4 = createElement(8);
+  ELEMENT element_5 = createElement(3);
+  ELEMENT element_6 = createElement(5);
+  ELEMENT element_7 = createElement(6);
+  ELEMENT element_8 = createElement(7);
+
+  insert(&list, &element_1);
+  insert(&list, &element_2);
+  insert(&list, &element_3);
+  insert(&list, &element_4);
+  insert(&list, &element_5);
+  insert(&list, &element_6);
+  insert(&list, &element_7);
+  insert(&list, &element_8);
+
+  show(&list);
+
+  return 0;
+}
